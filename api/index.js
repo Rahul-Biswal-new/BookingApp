2a40c782bf7618cdb0abcb4a21715454af0ce40f
Rahul <rahulbiswalnew@gmail.com>
2023-07-08 19:36:37 +0530

    model

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import userRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+
+
 const app = express();
 dotenv.config()
-
 
 const connect = async () =>{
 try{
@@ -14,7 +19,7 @@ try{
 }
 };
 
-
+// mongodb connection
 mongoose.connection.on("disconnected", ()=>{
     console.log("mongodb disconnected!");
 })
@@ -24,15 +29,22 @@ mongoose.connection.on("connected", ()=>{
 
 
 
-app.get('/', (req,res)=>{
-    res.send('<h1>Hello, homepage!</h1>'); 
-})
-app.get('/users', (req,res)=>{
-    res.send('<h1>Hello, users!</h1>'); 
-})
+// middlewares
+app.use('/api/auth', authRoute);
+app.use('/api/users', authRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', authRoute);
 
 
+// app.get('/', (req,res)=>{
+//     res.send('<h1>Hello, homepage!</h1>'); 
+// })
+// app.get('/users', (req,res)=>{
+//     res.send('<h1>Hello, users!</h1>'); 
+// })
 
+
+// listening port 
 app.listen(8800, ()=>{
     connect();
     console.log("connected to backend, on port 8800");
